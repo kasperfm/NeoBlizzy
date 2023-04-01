052abd3190704d5b05517b0d2ea98174fa8812b6
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2023-04-01 14:35:10 +0200

    Forgot to add a migration.

---
+++ resources/database/migrations/2023_03_28_162006_create_neoblizzy_oauth2_tokens_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('neoblizzy_oauth2_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->index();
+            $table->string('game', 32)->index();
+            $table->string('region', 2);
+            $table->integer('realm');
+            $table->integer('profile_id')->index();
+            $table->string('token');
+            $table->timestamp('expires_at')->nullable()->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('neoblizzy_oauth2_tokens');
+    }
+};
